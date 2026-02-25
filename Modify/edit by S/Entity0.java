93e7f1f404090cac4518458a0ea94db4d4377816
LonelyThic <LonelyThic@users.noreply.github.com>
2026-02-25 09:37:50 -0800

    yup

--- Modify/edit by S/Entity0.java
+++ Modify/edit by S/Entity0.java
@@ -1,8 +1,22 @@
 public class Entity0 extends Entity
 {    
+    private static final int INFINITY = 999;
+    private int id = 0;
+
     // Perform any necessary initialization in the constructor
     public Entity0()
     {
+        System.out.println("Entity0 constructor called");
+
+        for(int i=0;i<NetworkSimulator.NUMENTITIES;i++)
+            for(int j=0;j<NetworkSimulator.NUMENTITIES;j++)
+                distanceTable[i][j] = INFINITY;
+
+        for(int i=0;i<NetworkSimulator.NUMENTITIES;i++)
+            distanceTable[i][i] = NetworkSimulator.cost[id][i];
+
+        sendToNeighbors(id);
+        printDT();
     }
     
     // Handle updates when a packet is received.  Students will need to call
@@ -11,7 +25,30 @@ public class Entity0 extends Entity
     // the packet correctly.  Read the warning in NetworkSimulator.java for more
     // details.
     public void update(Packet p)
-    {        
+    {      
+        System.out.println("Entity0 receives packet from "+p.getSource());
+
+        boolean updated = false;
+        int src = p.getSource();
+
+        for(int dest=0; dest<NetworkSimulator.NUMENTITIES; dest++)
+        {
+            int newCost =
+                NetworkSimulator.cost[id][src] +
+                p.getMincost(dest);
+
+            if(newCost < distanceTable[dest][src])
+            {
+                distanceTable[dest][src] = newCost;
+                updated = true;
+            }
+        }
+
+        if(updated)
+        {
+            sendToNeighbors(id);
+            printDT();
+        }  
     }
     
     public void linkCostChangeHandler(int whichLink, int newCost)
---
+++ Modify/edit by S/README.md
@@ -0,0 +1,2 @@
+Work in progress
+in this folder. you can edit it or just edit the normal one. i made this for testing 
