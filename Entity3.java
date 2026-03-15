539c507ea492fb00badfae87076ba404bc511ab4
LonelyThic <LonelyThic@users.noreply.github.com>
2026-03-14 17:40:32 -0700

    Update Entity3.java
    entity3 java add getMinCost method and code

--- Entity3.java
+++ Entity3.java
@@ -47,6 +47,20 @@ public class Entity3 extends Entity
             printDT();
         }
     }
+
+    private int[] getMinCost()
+    {
+        int[] min=new int[NetworkSimulator.NUMENTITIES];
+
+        for(int i=0;i<NetworkSimulator.NUMENTITIES;i++)
+        {
+            min[i]=INFINITY;
+            for(int j=0;j<NetworkSimulator.NUMENTITIES;j++)
+                min[i]=Math.min(min[i],distanceTable[i][j]);
+        }
+        return min;
+    }
+
     
     public void linkCostChangeHandler(int whichLink, int newCost)
     {
