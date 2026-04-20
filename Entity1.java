77e932473a8c4483ba7cd0a92e16a685a55ed9d1
LonelyThic <LonelyThic@users.noreply.github.com>
2026-04-20 13:31:17 -0700

    Update Entity1.java

--- Entity1.java
+++ Entity1.java
@@ -1,21 +1,28 @@
 public class Entity1 extends Entity
 {    
+    // Constant representing infinity (no connection)
 	static final int INFINITY = 999;
+    // ID of this node (Entity1 → node 1)
     int id = 1;
     // Perform any necessary initialization in the constructor
     public Entity1()
     {
+        // Debug message to confirm constructor is called
 		System.out.println("Entity1 constructor called");
 
+        //Initialize entire distance table to INFINITY
+        // This means initially, no routes are known
         for(int i=0;i<NetworkSimulator.NUMENTITIES;i++)
             for(int j=0;j<NetworkSimulator.NUMENTITIES;j++)
                 distanceTable[i][j] = INFINITY;
 
+        //Set direct link costs (from cost matrix)
+        // distanceTable[i][i] = cost from this node to i
         for(int i=0;i<NetworkSimulator.NUMENTITIES;i++)
             distanceTable[i][i] = NetworkSimulator.cost[id][i];
 
-        sendToNeighbors();
-        printDT();
+        sendToNeighbors(); // Send initial distance vector to neighbors
+        printDT(); // Print initial distance table
     }
     
     // Handle updates when a packet is received.  Students will need to call
@@ -23,6 +30,8 @@ public class Entity1 extends Entity
     // send to update.  Be careful to construct the source and destination of
     // the packet correctly.  Read the warning in NetworkSimulator.java for more
     // details.
+
+    // Called when a packet is received from a neighbor
     public void update(Packet p)
     {
 		System.out.println("Entity1 receives packet from "+p.getSource());
